aba2e8e28a0aacee5580ca9b68272aea51aabae8
cap@CASA <cesar.alba@gmail.com>
2017-12-01 18:12:02 +0100

    Estadisticas e inicio del guesser 2

---
+++ SMACB/SMconstants.py
@@ -0,0 +1,9 @@
+
+MAXIMOextranjeros = 2
+MINIMOnacionales = 4
+
+PRESUPUESTOinicial=6500000.0
+PRECIOpunto=70000.0
+
+POSICIONES = ['posicion1','posicion3','posicion5']
+CUPOS = ['ExtraComunitario','Español','normal']
