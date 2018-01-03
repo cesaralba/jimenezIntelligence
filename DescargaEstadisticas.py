7bf41d5927246d9d7032739df1e8cfa489f51a11
cap@CASA <cesar.alba@gmail.com>
2018-01-03 15:03:28 +0100

    DescargaEstadisticas.py: + Opcion (-j justone) para bajar sólo un partido (para depurar) + Opcion (-e edicion) para bajar una edicion distinta de la de en curso

--- DescargaEstadisticas.py
+++ DescargaEstadisticas.py
@@ -5,44 +5,55 @@ from configargparse import ArgumentParser
 from mechanicalsoup import StatefulBrowser
 
 from SMACB.CalendarioACB import BuscaCalendario, CalendarioACB
+from SMACB.PartidoACB import PartidoACB
 from Utils.Web import ExtraeGetParams
 
 if __name__ == '__main__':
     parser = ArgumentParser()
     parser.add('-v', dest='verbose', action="count", env_var='SM_VERBOSE', required=False, default=0)
     parser.add('-d', dest='debug', action="store_true", env_var='SM_DEBUG', required=False, default=False)
-    parser.add('-i', dest='infile', type=str, env_var='SM_INFILE', required=False)
-    parser.add('-o', dest='outfile', type=str, env_var='SM_OUTFILE', required=False)
+    parser.add('-j', dest='justone', action="store_true", env_var='SM_JUSTONE', required=False, default=False)
 
-    # parser.add_argument('-i', '--input', type=str, required=False, dest='infile')
+    parser.add('-e', dest='edicion', action="store", env_var='SM_EDICION', required=False, default=None)
+    # parser.add('-c', dest='competicion', action="store", env_var='SM_COMPETICION', required=False, default=None)
+
+    # parser.add('-i', dest='infile', type=str, env_var='SM_INFILE', required=False)
+    # parser.add('-o', dest='outfile', type=str, env_var='SM_OUTFILE', required=False)
 
     args = parser.parse_args()
 
     browser = StatefulBrowser(soup_config={'features': "html.parser"}, raise_on_404=True, user_agent="SMparser",)
 
     sourceURL = BuscaCalendario(browser=browser, config=args)
-    paramsURL = ExtraeGetParams(sourceURL)
 
-    calendario = CalendarioACB(edition=paramsURL['cod_edicion'], urlbase=sourceURL)
+    if args.edicion is not None:
+        calendario = CalendarioACB(edition=args.edicion, urlbase=sourceURL)
+    else:
+        paramsURL = ExtraeGetParams(sourceURL)
+        calendario = CalendarioACB(edition=paramsURL['cod_edicion'], urlbase=sourceURL)
+
     # calendario = CalendarioACB(edition=56, urlbase=sourceURL)
     calendario.BajaCalendario(browser=browser, config=args)
+
     if 0:
         print(calendario.codigo2equipo)
         print(calendario.equipo2codigo)
         print(calendario.__dict__)
-    exit(1)
+        exit(1)
 
-    cal = CalendarioACB(config=args)
-    cal.BajaCalendario()
+    Partidos = {'descargados': set(), 'informacion': {}}
+    partidosBajados = set()
 
-#     if 'infile' in args and args.infile:
-#         sm.loadData(args.infile)
-#
-#     #sm = SuperManagerACB(config=args)
-#     sm.Connect()
-#
-#     sm.getSMstatus()
-#
-#     if sm.changed and ('outfile' in args and args.outfile):
-#         print("There were changes!")
-#         sm.saveData(args.outfile)
+    for partido in calendario.Partidos:
+        if partido in Partidos['descargados']:
+            continue
+
+        nuevoPartido = PartidoACB(**(calendario.Partidos[partido]))
+        nuevoPartido.DescargaPartido(home=None, browser=browser, config=args)
+
+        Partidos['descargados'].add(partido)
+        Partidos['informacion'][partido] = nuevoPartido
+        partidosBajados.add(partido)
+        print("PARTIDO:  ", nuevoPartido.__dict__)
+        if args.justone:
+            exit(1)
