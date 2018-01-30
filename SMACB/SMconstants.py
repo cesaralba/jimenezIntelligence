d06c1bd1eead6853f35e75b2d98a4d948d1a6ac6
calba@CAPvoyager <calba@tid.es>
2018-01-30 20:15:29 +0100

    SMACB/SMconstants.py: codificacion de textoÂ

--- SMACB/SMconstants.py
+++ SMACB/SMconstants.py
@@ -6,6 +6,6 @@ PRESUPUESTOinicial = 6500000.0
 PRECIOpunto = 70000.0
 
 POSICIONES = {'posicion1': 'Base', 'posicion3': 'Alero', 'posicion5': 'Pivot'}
-CUPOS = ['Extracomunitario', 'Espa��ol', 'normal']
+CUPOS = ['Extracomunitario', 'Español', 'normal']
 
 LISTACOMPOS = {'puntos': 'P', 'rebotes': 'REB-T', 'triples': 'T3-C', 'asistencias': 'A'}
