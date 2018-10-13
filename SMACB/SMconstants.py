286fab81666eeebfff8a7436c37f205265a98507
cap@CASA <cesar.alba@gmail.com>
2018-10-13 09:51:34 +0200

    01_guesser_1819 [WIP] Funciones de utilidad

--- SMACB/SMconstants.py
+++ SMACB/SMconstants.py
@@ -17,3 +17,15 @@ BONUSVICTORIA = 1.2
 def calculaValSuperManager(valoracion, haGanado=False):
     return round(
         decimal.Decimal.from_float(float(valoracion) * (BONUSVICTORIA if (haGanado and (valoracion > 0)) else 1.0)), 2)
+
+
+def buildPosCupoIndex():
+    indexResult = dict()
+
+    aux = 0
+    for pos in POSICIONES:
+        indexResult[pos] = dict()
+        for cupo in CUPOS:
+            indexResult[pos][cupo] = aux
+            aux += 1
+    return indexResult
