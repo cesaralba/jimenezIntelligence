7d71ec081e82ae8a077544d7b06baa16d8c3377d
cap@CASA <cesar.alba@gmail.com>
2018-11-11 00:42:00 +0100

    Cambiado para que cree 2 eggs: SMACB y Utils

--- setup.py
+++ setup.py
@@ -1,9 +1,17 @@
 from setuptools import find_packages, setup
 
 setup(
-    name="jimInt",
+    name="SMACB",
     version_format='0.1.dev{commitcount}+{gitsha}',
     setup_requires=['setuptools-git-version'],
     version="0.1",
-    packages=find_packages(exclude=['tests/*']),
+    packages=['SMACB'],
+)
+
+setup(
+    name="Utils",
+    version_format='0.1.dev{commitcount}+{gitsha}',
+    setup_requires=['setuptools-git-version'],
+    version="0.1",
+    packages=['Utils'],
 )
