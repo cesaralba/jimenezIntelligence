ffb176caeed8309f0d419785f10143a012409f72
cap@CASA <cesar.alba@gmail.com>
2019-09-22 13:26:41 +0200

    isort

--- SMACB/FichaJugador.py
+++ SMACB/FichaJugador.py
@@ -1,7 +1,6 @@
 import re
 from argparse import Namespace
-from time import gmtime, strptime
-from time import strftime
+from time import gmtime, strftime, strptime
 
 from Utils.Web import DescargaPagina, ExtraeGetParams, creaBrowser
 
