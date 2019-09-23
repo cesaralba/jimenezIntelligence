c0a674d038600936931a6e8e7227462ad616c85c
cap@CASA <cesar.alba@gmail.com>
2019-09-23 19:28:46 +0200

    Eliminada la obligatoriedad de -i en GetSuperManagerMerged.py

--- GetSuperManagerMerged.py
+++ GetSuperManagerMerged.py
@@ -15,7 +15,7 @@ if __name__ == '__main__':
     parser.add('-v', dest='verbose', action="count", env_var='SM_VERBOSE', required=False, default=0)
     parser.add('-d', dest='debug', action="store_true", env_var='SM_DEBUG', required=False, default=False)
 
-    parser.add('-i', dest='infile', type=str, env_var='SM_INFILE', required=True)
+    parser.add('-i', dest='infile', type=str, env_var='SM_INFILE', required=False)
     parser.add('-o', dest='outfile', type=str, env_var='SM_OUTFILE', required=False)
     parser.add('-t', dest='temporada', type=str, env_var='SM_TEMPORADA', required=True)
     parser.add('-l', dest='league', type=str, env_var='SM_LEAGUE', required=False, default=None)
