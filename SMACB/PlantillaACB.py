90570abdd7914bedeccb8c25e8e04779166328a1
cap@CASA <cesar.alba@gmail.com>
2019-11-18 08:55:52 +0100

    Añadida búsqueda de código de persona a partir de nombre

--- SMACB/PlantillaACB.py
+++ SMACB/PlantillaACB.py
@@ -1,7 +1,9 @@
+import re
 from argparse import Namespace
 from time import gmtime
 
-from Utils.Web import DescargaPagina, creaBrowser, MergeURL, getObjID
+from Utils.Web import DescargaPagina, MergeURL, creaBrowser, getObjID
+
 from .SMconstants import URL_BASE
 
 CLAVESFICHA = ['alias', 'nombre', 'lugarNac', 'fechaNac', 'posicion', 'altura', 'nacionalidad', 'licencia']
@@ -28,7 +30,7 @@ class PlantillaACB(object):
         # http://www.acb.com/club/plantilla/id/6/temporada_id/2016
         params = ['/club', 'plantilla', 'id', self.id]
         if self.edicion is not None:
-            params.append(['temporada_id', self.edicion])
+            params += ['temporada_id', self.edicion]
 
         urlSTR = "/".join(params)
 
@@ -46,6 +48,27 @@ class PlantillaACB(object):
                   'config': config}
 
         return PlantillaACB(**params)
+
+    def getCode(self, nombre, dorsal, esTecnico=False, esJugador=False):
+        setNombre = preparaNombreParaBuscar(nombre)
+
+        if esJugador:
+            for jCode, jData in self.data['jugadores'].items():
+                if jData['dorsal'] == dorsal:
+                    for jNombre in jData['nombre']:
+                        jDataSet = preparaNombreParaBuscar(jNombre)
+                        if len(setNombre.intersection(jDataSet)) > 0:
+                            return jCode
+        elif esTecnico:
+            for tCode, tData in self.data['tecnicos'].items():
+                for tNombre in tData['nombre']:
+                    tDataSet = preparaNombreParaBuscar(tNombre)
+                    if len(setNombre.intersection(tDataSet)) > 0:
+                        return tCode
+        else:
+            print("getCode: ¿Ni técnico ni jugador? (WTF)", nombre, dorsal, esTecnico, esJugador)
+
+        return None
 
 
 def descargaURLplantilla(urlPlantilla, home=None, browser=None, config=Namespace()):
@@ -85,8 +108,8 @@ def procesaPlantillaDescargada(plantDesc):
                 destClass = 'jugadores'
                 data['pos'] = jugArt.find("div", {"class": "posicion"}).get_text().strip()
                 data['nombre'].add(jugArt.find("div", {"class": "nombre"}).get_text().strip())
-            elif 'caja_entrenador_principal' in jugArt.attrs['class'] or 'caja_entrenador_asistente' in jugArt.attrs[
-                'class']:
+            elif ('caja_entrenador_principal' in jugArt.attrs['class']) or (
+                    'caja_entrenador_asistente' in jugArt.attrs['class']):
                 destClass = 'tecnicos'
                 if 'caja_entrenador_principal' in jugArt.attrs['class']:
                     data['nombre'].add(jugArt.find("div", {"class": "nombre"}).get_text().strip())
@@ -132,8 +155,26 @@ def procesaPlantillaDescargada(plantDesc):
 
 
 def encuentraUltEdicion(plantDesc):
+    """
+    Obtiene la última edición de la temporada del contenido de la página (lo extrae del selector de temporadas)
+    :param plantDesc:
+    :return:
+    """
     fichaData = plantDesc['data']
-    # <input id="select_temporada_id" name="select_temporada_id" type="hidden" value="2019"/><span class="mayusculas" id="select_temporada_titulo"> 
+
     result = fichaData.find("input", {"name": "select_temporada_id"}).attrs['value']
 
     return result
+
+
+def preparaNombreParaBuscar(nombre):
+    """
+    Tokeniza el nombre del jugador para compararlo con otros nombres conocidos de él (en la ficha)
+    :param nombre:
+    :return: set de palabras normalizado
+    """
+    patQuitaComa = r'^([^,]+)(\s*,.*)?$'
+
+    REquitaComa = re.match(patQuitaComa, nombre)
+    resNombre = REquitaComa.group(1)
+    return set(resNombre.lower().split(' '))
