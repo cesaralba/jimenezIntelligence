1a75138d16ee6f86978970a0017fbc6349a1260f
cap@CASA <cesar.alba@gmail.com>
2020-12-28 16:44:24 +0100

    * Utils/FechaHora.py pasado a pd.Datetime * Funcion para calcular la edad

--- Utils/FechaHora.py
+++ Utils/FechaHora.py
@@ -1,4 +1,12 @@
+from collections import namedtuple
+
+import numpy as np
+import pandas as pd
 from time import struct_time, strftime
+
+NEVER = pd.to_datetime("2030-12-31 00:00")
+
+Edad = namedtuple('Edad', ['delta', 'years', 'meses', 'dias', 'doys'])
 
 
 def Time2Str(timeref: struct_time):
@@ -13,3 +21,35 @@ def Time2Str(timeref: struct_time):
     result = strftime(format, timeref)
 
     return result
+
+
+def CumplePrevio(fechanac, fecharef):
+    diffyear = -1 if (fecharef.month, fecharef.day) < (fechanac.month, fechanac.day) else 0  # Es el del año anterior
+
+    fechaCump = f'{fecharef.year + diffyear:4}-{fechanac.month:2}-{fechanac.day:2}'
+    result = pd.to_datetime(fechaCump)
+
+    return result
+
+
+def CalcEdad(fechanac, fecharef=None):
+    """Calcula la edad. La cuenta de meses días, falla en el caso de bisiestos"""
+    if fecharef is None:
+        fecharef = pd.to_datetime("today")
+
+    datenac = fechanac.date()
+    dateref = fecharef.date()
+    cumple = CumplePrevio(fechanac=datenac, fecharef=dateref).date()
+
+    auxdiffyear = -1 if (fecharef.month, fecharef.day) < (fechanac.month, fechanac.day) else 0
+    yeardiff = fecharef.year - fechanac.year + auxdiffyear
+
+    auxDate = pd.Timedelta(dateref - datenac)
+
+    edadAux = {'delta': (dateref - datenac),
+               'years': yeardiff,
+               'meses': int(auxDate // np.timedelta64(1, 'M')) % 12,
+               'dias': int((auxDate % np.timedelta64(1, 'M')).days),
+               'doys': int((dateref - cumple).days)}
+
+    return Edad(**edadAux)
