db82c934b146ae34560189a7d695a8d1e8d2d74e
cap@CASA <cesar.alba@gmail.com>
2020-12-30 08:16:36 +0100

    Dulcificado el mensaje y recuperada la excepción si se trata de jugador

--- SMACB/PartidoACB.py
+++ SMACB/PartidoACB.py
@@ -106,12 +106,18 @@ class PartidoACB(object):
             if self.pendientes[l]:
                 for datosJug in self.pendientes[l]:
                     if datosJug['nombre'] == '':
-                        print("Datos insuficientes para encontrar ID. Partido: %s. %s" % (self, datosJug))
+                        datosJugTxt = "{localidad} Eq: '{equipo}' Dorsal: {dorsal} {posicion}".format(
+                            localidad=("Local" if datosJug['esLocal'] else "Visit"),
+                            equipo=datosJug['equipo'],
+                            dorsal=datosJug['dorsal'],
+                            posicion=("Jugador" if datosJug['esJugador'] else "Entrenador"))
+                        print(f"(W) Partido: {self} -> {datosJugTxt}: Datos insuficientes para encontrar ID.")
+                        newPendientes.append(datosJug)
+                        if datosJug['esJugador']:
+                            # Admitimos la pifia para entrenador pero no para jugadores
+                            raiser = True
+
                         continue
-                        # newPendientes.append(datosJug)
-                        # if datosJug['esJugador']:
-                        #     # Admitimos la pifia para entrenador pero no para jugadores
-                        #     raiser = True
                     else:
                         if cachedTeam is None:
                             cachedTeam = PlantillaACB(id=datosJug['IDequipo'], edicion=datosJug['temporada'])
