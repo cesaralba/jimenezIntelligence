783200fb4540d72cab1d25f75bb639b8cbfcc1b4
cap@CASA <cesar.alba@gmail.com>
2021-01-31 13:53:35 +0100

    New wish

--- SMACB/TemporadaACB.py
+++ SMACB/TemporadaACB.py
@@ -66,6 +66,8 @@ class TemporadaACB(object):
     Aglutina calendario y lista de partidos
     '''
 
+    #TODO: función __str__
+
     def __init__(self, **kwargs):
         self.competicion = kwargs.get('competicion', "LACB")
         self.edicion = kwargs.get('edicion', None)
