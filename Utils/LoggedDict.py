f0d4e113da746012de5f6db52c82ef6aaaee06fe
cap@CASA <cesar.alba@gmail.com>
2021-05-04 05:44:29 +0200

    * Creado DictOfLoggedDict

--- Utils/LoggedDict.py
+++ Utils/LoggedDict.py
@@ -72,3 +72,75 @@ class LoggedDict:
     def __len__(self):
         currData = [k for k, v in self.current.items() if not v.isDeleted()]
         return len(currData)
+
+
+class DictOfLoggedDict:
+    def __init__(self, exclusions: set = None):
+
+        if exclusions is not None and not isinstance(exclusions, (set, list, tuple)):
+            raise TypeError(f"DictOfLoggedDict: expected set/list/tuple for exclusions: {exclusions}")
+
+        self.current = dict()
+        self.exclusions = exclusions or set()
+        self.timestamp = gmtime()
+
+    def __getitem__(self, item):
+        return self.current.__getitem__(item)
+
+    def __setitem__(self, k, v):
+        currVal = self.current.get(k, LoggedDict(exclusions=self.exclusions))
+        changes = currVal.update(v)
+
+        self.current[k] = currVal
+        return changes
+
+    def get(self, key, default=None):
+        return self.current.get(key, default)
+
+    def update(self, newValues, timestamp=None):
+        changeTime = timestamp or gmtime()
+        result = []
+
+        if not isinstance(newValues, dict):
+            raise TypeError("LoggedDict.DictOfLoggedDict.update: expected dict")
+
+        for k, v in newValues.items():
+            currVal = self.current.get(k, LoggedDict(exclusions=self.exclusions))
+
+            r1 = currVal.update(v, timestamp=changeTime)
+
+            if r1:
+                self.current[k] = currVal
+
+            result.append(r1)
+
+        return any(result)
+
+    def purge(self, keys2delete, timestamp=None):
+        changeTime = timestamp or gmtime()
+        result = []
+
+        for k in keys2delete:
+            if k in self.exclusions:
+                continue
+            if k in self.current:
+                r1 = self.current[k].clear(timestamp=changeTime)
+                result.append(r1)
+
+        return any(result)
+
+    def addExclusion(self, *kargs):
+        self.exclusions.update(set(kargs))
+
+        for k in self.current:
+            self.current[k].addExclusion(set(kargs))
+        # TOTHINK: Qué hacer con los valores almacenados y que han quedado excluidos?
+
+    def removeExclusion(self, *kargs):
+        self.exclusions.remove(set(kargs).intersection(self.exclusions))
+
+        for k in self.current:
+            self.current[k].removeExclusion(set(kargs))
+
+    def __len__(self):
+        return len(self.current)
