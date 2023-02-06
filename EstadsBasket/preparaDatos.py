2803e0406f42369faf35d075c5ff6b3f90c28824
cap@CASA <cesar.alba@gmail.com>
2023-02-06 20:13:20 +0100

    EstadsBasket/preparaDatos.py: long live cartesian product

--- EstadsBasket/preparaDatos.py
+++ EstadsBasket/preparaDatos.py
@@ -1,39 +1,57 @@
-import numpy as np
+from itertools import product
 
 import pandas as pd
 
-from SMACB.Constants import LocalVisitante
+from SMACB.Constants import LocalVisitante,EqRival
 from SMACB.TemporadaACB import TemporadaACB
 
-COLFECHAPARTIDO=('Info','fechaPartido')
+COLFECHAPARTIDO = ('Info', 'fechaPartido')
+ABREVPAIRS = {'LV': LocalVisitante, 'ER': EqRival}
 
-def dfPartidos2serieFechas(dfPartidos: pd.DataFrame, colFecha=COLFECHAPARTIDO, abrEq=None, datosTemp: TemporadaACB=None):
+COLSINFO4STATS = [('Info', 'VictoriaLocal'), ('Info', 'Ptot'), ('Info', 'Ftot'), ('Info', 'POStot'),
+                  ('Info', 'ratio40min')]
+
+COLSSTATEQ4STATS = ['haGanado', 'convocados', 'utilizados', 'Segs', 'P', 'T2-C', 'T2-I', 'T2%', 'T3-C', 'T3-I', 'T3%',
+                    'T1-C', 'T1-I', 'T1%', 'REB-T', 'R-D', 'R-O', 'A', 'BR', 'BP', 'TAP-F', 'TAP-C', 'FP-F', 'FP-C',
+                    'TC-I', 'TC-C', 'TC%', 'POS', 'OER', 'OERpot', 'EffRebD', 'EffRebO', 't2/tc-I', 't3/tc-I',
+                    't2/tc-C', 't3/tc-C', 'eff-t2', 'eff-t3', 'ppTC', 'A/TC-C', 'A/BP', 'RO/TC-F', 'P_por40m',
+                    'T2-C_por40m', 'T2-I_por40m', 'T3-C_por40m', 'T3-I_por40m', 'T1-C_por40m', 'T1-I_por40m',
+                    'REB-T_por40m', 'R-D_por40m', 'R-O_por40m', 'A_por40m', 'BR_por40m', 'BP_por40m', 'TAP-F_por40m',
+                    'TAP-C_por40m', 'FP-F_por40m', 'FP-C_por40m', 'V_por40m', 'TC-I_por40m', 'TC-C_por40m',
+                    'POS_por40m', 'P_porPos', 'T2-C_porPos', 'T2-I_porPos', 'T3-C_porPos', 'T3-I_porPos', 'A_porPos',
+                    'BR_porPos', 'BP_porPos', 'TAP-F_porPos', 'TAP-C_porPos', 'FP-F_porPos', 'FP-C_porPos',
+                    'TC-I_porPos', 'TC-C_porPos']
+
+
+def dfPartidos2serieFechas(dfPartidos: pd.DataFrame, colFecha=COLFECHAPARTIDO, abrEq=None,
+                           datosTemp: TemporadaACB = None):
     listaPartidos = dfPartidos
     if abrEq:
         if datosTemp is None:
-            raise ValueError(f"dfPartidos2serieFechas: si se suministra abreviatura '{abrEq}' se requiere info de temporada")
+            raise ValueError(
+                f"dfPartidos2serieFechas: si se suministra abreviatura '{abrEq}' se requiere info de temporada")
 
-        if  not len(datosTemp.tradEquipos['c2i'][abrEq]):
+        if not len(datosTemp.tradEquipos['c2i'][abrEq]):
             raise KeyError(f"dfPartidos2serieFechas: '{abrEq}' desconocida en temporada")
 
         idEq = list(datosTemp.tradEquipos['c2i'][abrEq])[0]
-        listaPartidos = dfPartidos[teamMatch(dfPartidos,abrEq)]
+        listaPartidos = dfPartidos[teamMatch(dfPartidos, idEq, field='id', teamOnly=True)]
 
-    if (colFecha in dfPartidos.columns):
-        result = listaPartidos[colFecha].unique()
-    elif (colFecha in dfPartidos.index.names):
-        if isinstance(dfPartidos.index,pd.Index):
-            result = dfPartidos.index.to_series().unique()
-        elif isinstance(dfPartidos.index,pd.MultiIndex):
-            result = dfPartidos.index.to_frame(allow_duplicates=False)
+    if (colFecha in listaPartidos.columns):
+        auxResult = pd.Series(listaPartidos[colFecha].unique())
+    elif (colFecha in listaPartidos.index.names):
+        if isinstance(listaPartidos.index, pd.Index):
+            auxResult = pd.Series(listaPartidos.index.to_series().unique())
+        elif isinstance(listaPartidos.index, pd.MultiIndex):
+            auxResult = pd.Series(listaPartidos.index.to_frame(allow_duplicates=False))
         else:
             raise TypeError("dfPartidos2serieFechas: tipo desconocido de index")
     else:
         raise KeyError(f"dfPartidos2serieFechas: columna desconocida '{colFecha}'")
 
-    result.sort()
+    result = auxResult.groupby(auxResult.dt.date).max().sort_values()
 
-    return pd.Series(result)
+    return result
 
 
 def preparaEstadisticas(partidosLV: pd.DataFrame):
@@ -61,42 +79,58 @@ def preparaEstadisticas(partidosLV: pd.DataFrame):
     return result
 
 
-def getAbrevPair(df: pd.DataFrame):
-    if (('Local','abrev') in df.columns) and (('Visitante','abrev') in df.columns):
-        return [('Local','abrev'),('Visitante','abrev')]
-    elif (('Eq','abrev') in df.columns) and (('Rival','abrev') in df.columns):
-        return [('Eq','abrev'),('Rival','abrev')]
+def df2KEYPAIR(df):
+    field = 'abrev'
+    for dfType, pairMain in ABREVPAIRS.items():
+        pair = list(product(pairMain, [field]))
+
+        if len(set(df.columns.to_list()).intersection(pair)) == len(pair):
+            result = dfType
+            break
     else:
-        raise ValueError('getAbrevPair: tipo no reconocido')
+        raise ValueError('df2KEYPAIR: tipo de df no reconocido')
 
-
-def teamMatch(df,abrev):
-    pairKeys = getAbrevPair(df)
-    result=(df[pairKeys]==abrev).any(axis=1)
     return result
 
-def getMarkerMatch(df,abrev1,abrev2,fill=False,filler=''):
-    result=pd.Series(index=df.index,dtype='object')
-    result[teamMatch(df,abrev1)]=abrev1
-    result[teamMatch(df,abrev2)]=abrev2
-    result[(teamMatch(df,abrev1)&teamMatch(df,abrev2))]='prec'
+
+def getAbrevPair(df: pd.DataFrame, field='abrev', teamOnly: bool = False):
+    resultType = df2KEYPAIR(df)
+    resultPair = list(product(ABREVPAIRS[resultType], [field]))
+    result = [resultPair[0]] if (teamOnly and (resultType == 'ER')) else resultPair
+
+    return result
+
+
+def teamMatch(df, x, field='abrev', teamOnly=False):
+    pairKeys = getAbrevPair(df, field=field, teamOnly=teamOnly)
+    result = (df[pairKeys] == x).iloc[:, 0] if len(pairKeys) == 1 else (df[pairKeys] == x).any(axis=1)
+
+    return result
+
+
+def getMarkerMatch(df, abrev1, abrev2, fill=False, filler=''):
+    result = pd.Series(index=df.index, dtype='object')
+    result[teamMatch(df, abrev1)] = abrev1
+    result[teamMatch(df, abrev2)] = abrev2
+    result[(teamMatch(df, abrev1) & teamMatch(df, abrev2))] = 'prec'
 
     if fill:
-        result=result.fillna(filler)
+        result = result.fillna(filler)
     return result
 
 
-def calculaEstadisticosPartidos(dfPartidos: pd.DataFrame,campoFecha=COLFECHAPARTIDO,listafechas=None):
-
+def calculaEstadisticosPartidos(dfPartidos: pd.DataFrame, campoFecha=COLFECHAPARTIDO, listafechas=None):
+    dfKeyPair = df2KEYPAIR(dfPartidos)
+    campos_STATS = COLSINFO4STATS + sorted(list(product(ABREVPAIRS[dfKeyPair], COLSSTATEQ4STATS)))
     if campoFecha is not None and (campoFecha not in dfPartidos.columns) and (campoFecha not in dfPartidos.index.names):
         raise KeyError(f"calculaEstadisticosPartidos: la clave {campoFecha} no está en las columnas del DF")
 
     if campoFecha in dfPartidos.columns:
         fechasRef = dfPartidos[[campoFecha]]
     elif campoFecha in dfPartidos.index.names:
-        if isinstance(dfPartidos.index,pd.Index):
+        if isinstance(dfPartidos.index, pd.Index):
             fechasRef = dfPartidos.index.to_series()
-        elif isinstance(dfPartidos.index,pd.MultiIndex):
+        elif isinstance(dfPartidos.index, pd.MultiIndex):
             fechasRef = dfPartidos.index.to_frame(allow_duplicates=True)[[campoFecha]]
         else:
             raise TypeError("dfPartidos2serieFechas: tipo desconocido de index")
@@ -105,10 +139,10 @@ def calculaEstadisticosPartidos(dfPartidos: pd.DataFrame,campoFecha=COLFECHAPART
 
     auxResult = []
     for t in fechasWork:
-        newRow = dfPartidos[fechasRef<=t].describe(percentiles=[0.5]).unstack().T
+        newRow = dfPartidos[fechasRef <= t][campos_STATS].describe(percentiles=[0.5]).unstack().T
 
         auxResult.append(newRow)
 
-    result = pd.DataFrame(data=auxResult,index=fechasWork)
+    result = pd.DataFrame(data=auxResult, index=fechasWork)
 
     return result
