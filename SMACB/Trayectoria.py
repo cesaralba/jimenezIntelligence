b554784dac96b488f098f0cb6534017c951b10ec
cap@CASA <cap@CASA>
2025-03-15 11:36:21 +0100

    Implementación de trayectoria de jugador

--- SMACB/Trayectoria.py
+++ SMACB/Trayectoria.py
@@ -1,5 +1,5 @@
-from collections import namedtuple
-from typing import Optional, Set, List
+from collections import namedtuple, defaultdict
+from typing import Optional, Set, List, Dict, Tuple
 
 import bs4
 from CAPcore.Web import DownloadedPage
@@ -9,6 +9,52 @@ from Utils.Web import getObjID
 
 TempClubInfoBasic = namedtuple('TempClubInfoBasic', ['tempId', 'clubId', 'clubName'])
 TempClubInfo = namedtuple('TempClubInfo', ['tempId', 'tempName', 'clubId', 'clubName'])
+ResEstanciasTray = namedtuple('ResEstanciasTray', ['clubId', 'numTemps', 'numPeriods', 'periodos'])
+
+
+class Trayectoria:
+    def __init__(self, trayectoria: List[TempClubInfo]):
+        self.trayectoria: List[EstanciaClub] = construyeEstanciaList(trayectoria)
+
+    def tempsACB(self):
+        auxSet = set()
+        for est in self.trayectoria:
+            auxSet.update(est.tempIDs)
+        result = len(auxSet)
+        return result
+
+    def tempsEnClub(self, ultimoPeriodo: bool = True) -> [int, Tuple[int, int]]:
+        """
+        :param ultimoPeriodo:
+        :return:
+        """
+        ultEst = self.trayectoria[-1]
+        if ultimoPeriodo:
+            return ultEst.numTemps
+        resEstancias = self.resumenTrayectoria()[ultEst.clubId]
+        return (resEstancias.numTemps, resEstancias.numPeriods)
+
+    def resumenTrayectoria(self) -> Dict[str, ResEstanciasTray]:
+        auxTrayect = defaultdict(lambda: {'clubId': None, 'numTemps': 0, 'numPeriods': 0, 'periodos': []})
+
+        for est in self.trayectoria:
+            auxTrayect[est.clubId]['clubId'] = est.clubId
+            auxTrayect[est.clubId]['numTemps'] += est.numTemps
+            auxTrayect[est.clubId]['numPeriods'] += 1
+            auxTrayect[est.clubId]['periodos'].append(est.buildPeriodoStr())
+
+        result = {k: ResEstanciasTray(**v) for k, v in auxTrayect.items()}
+
+        return result
+
+    @classmethod
+    def fromWebPage(cls, data: DownloadedPage):
+        datosTrayectoria = extraeTrayectoria(data)
+
+        if datosTrayectoria is None:
+            return None
+
+        return cls(datosTrayectoria)
 
 
 class EstanciaClub:
@@ -18,6 +64,7 @@ class EstanciaClub:
         self.numTemps: int = 0
         self.clubId: Optional[str] = None
         self.clubNames: Set[str] = set()
+        self.tempIDs: Set[str] = set()
 
         ClavesValidasEstancia = {'tempIni', 'tempFin', 'clubId', 'numTemps', 'clubName'}
         badKeys = set(kwargs.keys()).difference(ClavesValidasEstancia)
@@ -34,6 +81,7 @@ class EstanciaClub:
                 self.clubNames.add(v)
                 continue
             setattr(self, k, v)
+        self.tempIDs.add(kwargs['tempFin'])
 
     def esClub(self, clubId: str) -> bool:
         return self.clubId == clubId
@@ -63,17 +111,25 @@ class EstanciaClub:
         self.tempFin = other.tempId
         self.numTemps += 1
         self.clubNames.add(other.clubName)
+        self.tempIDs.add(other.tempId)
 
         return self
+
+    def buildPeriodoStr(self) -> str:
+        priYear = self.tempIni
+        ultYear = (int(self.tempFin) + 1) % 100
+
+        result = f"{priYear}-{ultYear:02}"
+
+        return result
 
     def __str__(self):
         if self.numTemps == 0:
             return "null"
 
+        tempsTag = f"({self.numTemps} temp{'s' if self.numTemps > 1 else ''})"
         targetName = sortedByStringLength(self.clubNames)[0]
-        priYear = self.tempIni
-        ultYear = (int(self.tempFin) + 1) % 100
-        return f"{priYear}-{ultYear:02} ({self.numTemps} temps) [{self.clubId}] {targetName}"
+        return f"{self.buildPeriodoStr()} {tempsTag} [{self.clubId}] {targetName}"
 
     __repr__ = __str__
 
