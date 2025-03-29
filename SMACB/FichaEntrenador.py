65ff94f5c3f9137e9771ddd449e9e50d524a6d98
cap@CASA <cap@CASA>
2025-03-29 11:46:29 +0100

    WIP Integración entrenador en Temporada

--- SMACB/FichaEntrenador.py
+++ SMACB/FichaEntrenador.py
@@ -1,8 +1,19 @@
 from .FichaPersona import FichaPersona
 
+sentinel=object
 
 class FichaEntrenador(FichaPersona):
-    def __init__(self,**kwargs):
-        self.tipoFicha = 'entrenador'
+    def __init__(self, **kwargs):
+        changesInfo = {'NuevoEntrenador': True}
 
-        super(FichaEntrenador,self).__init__(tipoFicha='entrenador',**kwargs)
+        dataConocida = {'tipoFicha': 'entrenador', 'changesInfo': changesInfo}
+        dataConocida.update(kwargs)
+        super().__init__(**dataConocida)
+
+
+    def actualizaBio(self, data)->bool:
+        # Los entrenadores tienen las mismas cosas que FichaPersona, los jugadores necesitarán tratamiento específico
+        result=False
+        result |= self.actualizaBioBasic(**data)
+
+        return result
--- SMACB/FichaPersona.py
+++ SMACB/FichaPersona.py
@@ -1,87 +1,110 @@
 import logging
-from typing import Optional
+from time import gmtime
+from typing import Optional, Set, Dict
 
 import bs4
 from CAPcore.Web import mergeURL, DownloadedPage, downloadPage
 
 from Utils.ParseoData import procesaCosasUtilesPlantilla, findLocucionNombre
-from Utils.Web import prepareDownloading
+from Utils.Web import prepareDownloading, getObjID
 from .Constants import URL_BASE, URLIMG2IGNORE
+from .PartidoACB import PartidoACB
 from .Trayectoria import Trayectoria
 
-VALIDTYPES = {'jugador', 'entrenador'}
+VALIDPERSONATYPES = {'jugador', 'entrenador'}
 
+PERSONABASICTAGS={'audioURL', 'nombre', 'alias', 'lugarNac', 'fechaNac', 'nacionalidad', }
+
+sentinel=object
 
 class FichaPersona:
     def __init__(self, **kwargs):
-        changesInfo = {'NuevoJugador': True}
-        self.timestamp = kwargs.get('timestamp', None)
+        changesInfo = kwargs.get('changesInfo', {})
+        self.timestamp = kwargs.get('timestamp', gmtime())
 
         if 'id' not in kwargs:
-            raise ValueError(f"Ficha nueva sin 'id': {kwargs}")
-        self.id = kwargs.get('id', None)
-        self.URL = kwargs.get('URL', None)
-        self.audioURL = kwargs.get('audioURL', None)
-        self.tipoFicha: Optional[str] = None
-        self.sinDatos: Optional[bool] = None
+            raise ValueError(f"Ficha nueva sin 'persID': {kwargs}")
+        self.persID = kwargs['id']
+        tipoPers = kwargs.get('tipoFicha', None)
+        if tipoPers not in VALIDPERSONATYPES:
+            raise ValueError(f"Persona Id '{self.persID}' Tipo '{tipoPers}' no válido. Aceptados: {VALIDPERSONATYPES}")
+        self.tipoFicha: Optional[str] = tipoPers
+        self.URL: Optional[str] = None
         self.trayectoria: Optional[Trayectoria] = None
 
-        self.nombre = kwargs.get('nombre', None)
-        self.alias = kwargs.get('alias', self.nombre)
-        self.lugarNac = kwargs.get('lugarNac', None)
-        self.fechaNac = kwargs.get('fechaNac', None)
-        self.nacionalidad = kwargs.get('nacionalidad', None)
+        self.audioURL: Optional[str] = None
+        self.nombre: Optional[str] = None
+        self.alias: Optional[str] = None
+        self.lugarNac: Optional[str] = None
+        self.fechaNac: Optional[str] = None
+        self.nacionalidad: Optional[str] = None
+        self.nombresConocidos: Set[str] = set()
+        self.fotos: Set[str] = set()
+        self.ultClub: Optional[str] = None
 
-        self.nombresConocidos = set()
-        self.urlConocidas = set()
-        self.fotos = set()
+        self.partsTemporada: PartidosClub = PartidosClub(persID=self.persID, tipoFicha=self.tipoFicha, clubId=None)
+        self.partsClub: Dict[str, PartidosClub] = {}
 
-        self.ultClub = None
+        self.primPartidoP: Optional[str] = None
+        self.ultPartidoP: Optional[str] = None
+        self.primPartidoT: Optional[str] = None
+        self.ultPartidoT: Optional[str] = None
+        self.partidos: Set[str] = set()
+        self.equipos: Set[str] = set()
 
-        self.primPartidoP = None
-        self.ultPartidoP = None
-        self.primPartidoT = None
-        self.ultPartidoT = None
-        self.partidos = set()
-        self.equipos = set()
+        self.actualizaBioBasic(**kwargs)
+
+    def actualizaBio(self, data,changeInfo=sentinel):
+        raise NotImplemented("actualizaBio tiene que estar en las clases derivadas")
+
+    def actualizaBioBasic(self, **kwargs)->bool:
+        camposAmirar = {'URL', 'audioURL', 'nombre', 'alias', 'lugarNac', 'fechaNac', 'nacionalidad'}
+
+        result = False
+        for k in camposAmirar:
+            if k not in kwargs:
+                continue
+            if getattr(self, k) != kwargs[k]:
+                result |= True
+                setattr(self, k, kwargs[k])
 
         if self.nombre is not None:
-            self.nombresConocidos.add(self.nombre)
+            if self.nombre not in self.nombresConocidos:
+                result |= True
+                self.nombresConocidos.add(self.nombre)
+
         if self.alias is not None:
-            self.nombresConocidos.add(self.alias)
+            if self.alias not in self.nombresConocidos:
+                result |= True
+                self.nombresConocidos.add(self.alias)
 
-        if self.URL is not None:
-            self.urlConocidas.add(self.URL)
-
-        self.updateFoto(urlFoto=kwargs.get('urlFoto', None), urlBase=self.URL, changeDict=changesInfo)
-
+        result|=self.updateFoto(urlFoto=kwargs.get('urlFoto', None), urlBase=self.URL)
         ultClub = kwargs.get('club', None)
         if ultClub is not None:
             self.equipos.add(ultClub)
             self.ultClub = ultClub
+        return result
 
     def buildURL(self):
         if self.tipoFicha is None:
             raise ValueError("buildURL: type of data unset")
-        if self.tipoFicha not in VALIDTYPES:
-            raise ValueError(f"buildURL: unknown type of data. Valid ones: {', '.join(sorted(VALIDTYPES))}")
+        if self.tipoFicha not in VALIDPERSONATYPES:
+            raise ValueError(f"buildURL: unknown type of data. Valid ones: {', '.join(sorted(VALIDPERSONATYPES))}")
 
-        newPathList = ['', self.tipoFicha, 'temporada-a-temporada', 'id', self.id]
+        newPathList = ['', self.tipoFicha, 'temporada-a-temporada', 'id', self.persID]
         newPath = '/'.join(newPathList)
 
         result = mergeURL(URL_BASE, newPath)
 
         return result
 
-    def updateFoto(self, urlFoto: Optional[str], urlBase: Optional[str], changeDict: Optional[dict] = None):
+    def updateFoto(self, urlFoto: Optional[str], urlBase: Optional[str]):
         changes = False
 
         if urlFoto is not None and urlFoto not in URLIMG2IGNORE:
             changes = True
             newURL = mergeURL(urlBase, urlFoto)
             self.fotos.add(newURL)
-            if changeDict:
-                changeDict['urlFoto'] = ("", "Nueva")
         return changes
 
     def descargaPagina(self, home=None, browser=None, config=None) -> Optional[DownloadedPage]:
@@ -99,8 +122,120 @@ class FichaPersona:
 
         return result
 
-    def update(self):
-        pass
+    @classmethod
+    def fromURL(cls, url: str, datos: Optional[dict] = None, home=None, browser=None, config=None):
+
+        persId = getObjID(url, 'ver', None)
+        if persId is None:
+            raise ValueError(f"FichaPersona.fromURL: incapaz de sacar ID de f'{url}'. Buscados 'ver' e 'persID'")
+        newData = {'persID': persId}
+        if datos is not None:
+            newData.update(datos)
+
+        result = cls(**newData)
+        result.actualizaDatosWeb(browser, config, home)
+
+        return result
+
+    def ficha2dict(self) -> Dict[str,str]:
+        result = {k:getattr(self,k) for k in PERSONABASICTAGS}
+        return result
+
+    def nuevoPartido(self, partido: PartidoACB) -> bool:
+        """
+        Actualiza información relativa a partidos jugados
+        :param partido: OBJETO partidoACB
+        :return: Si ha cambiado el objeto o no
+        """
+        result = False
+
+        dicTipo = partido.Jugadores if self.tipoFicha == 'jugador' else partido.Entrenadores
+
+        if self.persID not in dicTipo:
+            raise ValueError(
+                f"{self.tipoFicha.capitalize()}: '{self.nombre}' ({self.persID}) no ha jugado partido {partido.url}")
+
+        datosPersPart = dicTipo[self.persID]
+        eqPersona = datosPersPart['IDequipo']
+
+        result |= self.partsTemporada.addPartido(persona=self, partido=partido)
+
+        if eqPersona not in self.partsClub:
+            self.partsClub[eqPersona] = PartidosClub(persID=self.persID, tipoFicha=self.tipoFicha, clubId=eqPersona)
+            result = True
+
+        result |= self.partsClub[eqPersona].addPartido(persona=self, partido=partido)
+
+        return result
+
+    def actualizaDatosWeb(self, browser=None, config=None, home=None) -> bool:
+        result =False
+        pag = self.descargaPagina(home, browser, config)
+        if pag is None:
+            return False
+        datosPersona = extraeDatosPersonales(datosPag=pag)
+        result |= self.actualizaBio(datosPersona)
+        if self.trayectoria is None:
+            self.trayectoria = Trayectoria.fromWebPage(data=pag)
+            result |= True
+
+        return result
+
+
+class PartidosClub:
+    def __init__(self, persID: str, tipoFicha: str, clubId: Optional[str]):
+        self.persID: str = persID
+        self.tipoFicha: str = tipoFicha
+        self.clubID: Optional[str] = clubId
+        self.equipos: Set[str] = set()
+
+        self.primPartidoP: Optional[str] = None
+        self.ultPartidoP: Optional[str] = None
+        self.primPartidoT: Optional[str] = None
+        self.ultPartidoT: Optional[str] = None
+        self.partidos: Set[str] = set()
+
+    def addPartido(self, persona: FichaPersona, partido: PartidoACB) -> bool:
+        """
+        Actualiza información relativa a partidos jugados
+        :param partido: OBJETO partidoACB
+        :return: Si ha cambiado el objeto o no
+        """
+
+        dicTipo = partido.Jugadores if persona.tipoFicha == 'jugador' else partido.Entrenadores
+
+        if self.persID not in dicTipo:
+            raise ValueError(
+                f"{persona.tipoFicha.capitalize()}: '{persona.nombre}' ({self.persID}) no ha jugado partido "
+                f"{partido.url}")
+
+        if partido.url in self.partidos:
+            return False
+
+        datosPersPart = dicTipo[self.persID]
+        eqJugador = datosPersPart['IDequipo']
+        if self.clubID is None:
+            self.equipos.add(eqJugador)
+        else:
+            if eqJugador != self.clubID:
+                raise ValueError(
+                    f"{persona.tipoFicha.capitalize()}: '{persona.nombre}' ({self.persID}) Añadiendo {partido.url} a "
+                    f"registro incorrecto. Registro: '{self.clubID}'. Partido: '{eqJugador}'")
+
+        self.partidos.add(partido.url)
+
+        if persona.ultClub is None:
+            persona.ultClub = datosPersPart['IDequipo']
+
+        if (self.primPartidoT is None) or (partido.fechaPartido < self.primPartidoT):
+            self.primPartidoP = partido.url
+            self.primPartidoT = partido.fechaPartido
+
+        if (self.ultPartidoT is None) or (partido.fechaPartido > self.ultPartidoT):
+            self.ultPartidoP = partido.url
+            self.ultPartidoT = partido.fechaPartido
+        return True
+
 
 def extraeDatosPersonales(datosPag: Optional[DownloadedPage], datosPartido: Optional[dict] = None):
     auxResult = {}
